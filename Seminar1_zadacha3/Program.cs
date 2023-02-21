bc05a48bfbcf7bec87b9e78b4e3f8bc3752f7e58
Наталья <dream301220@outlook.com>
2023-02-21 11:18:55 +0300

    Задача 3

---
+++ Seminar1_zadacha3/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+void FillArray(int[] collection)
+{
+    number = collection.Length;
+    collection[0] = 1;
+    int index = 1;
+    while (index < number)
+    {
+        collection[index] = index + 1;
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        position++;
+    }
+}
+int[] array = new int[number];
+FillArray(array);
+PrintArray(array);
+//Console.Write(string.Join(", ",array.Select(x =>x.ToString())));
+int[] evenArr = array.Where(x => x % 2 == 0).ToArray();
+Console.WriteLine(string.Join(", ", evenArr.Select(x => x.ToString())));
