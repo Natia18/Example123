60f0ea29177bce009d5f7ad5526c7ad0431a4e7c
Наталья <dream301220@outlook.com>
2023-02-21 10:33:31 +0300

    Найти максимальное число

---
+++ Seminar1_zadacha1/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine("Максимальное число: " + number1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + number3);
+    }
+}
+else if(number2 > number3)
+{
+    Console.WriteLine("Максимальное число: " + number2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + number3);
+}
